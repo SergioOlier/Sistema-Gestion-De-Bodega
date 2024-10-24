52d7585d090a0899df274ea24038de1022a1f0c2
SergioOlier <solierm17@gmail.com>
2024-10-24 15:35:18 -0500

    Add: Clase Proveedor

---
+++ Proveedor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestionDeBodega
+{
+    public class Proveedor
+    {
+        public string ID { get; set; }
+        public string Nombre { get; set; }
+        public List<Producto> Productos { get; set; }
+
+        public Proveedor(string id, string nombre)
+        {
+            ID = id;
+            Nombre = nombre;
+            Productos = new List<Producto>();
+        }
+
+        public void AgregarProducto(Producto producto)
+        {
+            Productos.Add(producto);
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {ID}, Nombre: {Nombre}";
+        }
+    }
+
+}
