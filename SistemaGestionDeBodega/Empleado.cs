0a0b365042ed5c79dc6a653e5246e07310c05c31
SergioOlier <solierm17@gmail.com>
2024-10-24 15:40:16 -0500

    Change: Organizar proyecto

---
+++ SistemaGestionDeBodega/Empleado.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestionDeBodega
+{
+    public class Empleado
+    {
+        public string ID { get; set; }
+        public string Nombre { get; set; }
+
+        public Empleado(string id, string nombre)
+        {
+            ID = id;
+            Nombre = nombre;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {ID}, Nombre: {Nombre}";
+        }
+    }
+
+    public class Administrador : Empleado
+    {
+        public Administrador(string id, string nombre) : base(id, nombre) { }
+
+        public void GestionarEmpleados()
+        {
+            Console.WriteLine("Gestionando empleados...");
+        }
+    }
+
+    public class EncargadoAlmacen : Empleado
+    {
+        public EncargadoAlmacen(string id, string nombre) : base(id, nombre) { }
+
+        public void GestionarInventario()
+        {
+            Console.WriteLine("Gestionando inventario...");
+        }
+    }
+
+}
